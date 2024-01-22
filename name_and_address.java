3de7831a341c593ed21dd891516a57f67317043b
addison93 <157327192+addison93@users.noreply.github.com>
2024-01-21 18:54:33 -0700

    Add files via upload

---
+++ name_and_address.java
@@ -0,0 +1,16 @@
+package name_and_address;
+
+public class name_and_address {
+	public static void main(String[] args) {
+		String nameFirst = "Elliot";
+		String nameLast = "Alderson";
+		String addressStreet = "217 E Broadway";
+		String addressCity = "New York, NY";
+		String addressZip = "10002";
+		
+		System.out.println(nameFirst + " " + nameLast);
+		System.out.println(addressStreet);
+		System.out.println(addressCity + " " + addressZip);
+				
+	}
+}
